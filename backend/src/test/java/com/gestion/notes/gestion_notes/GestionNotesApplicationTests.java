e5be0bc4f23075288c34433679f376155247f752
cedwics10 <cedric.von@outlook.fr>
2025-03-18 16:15:19 +0100

    Gestion notes

---
+++ backend/src/test/java/com/gestion/notes/gestion_notes/GestionNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.notes.gestion_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
